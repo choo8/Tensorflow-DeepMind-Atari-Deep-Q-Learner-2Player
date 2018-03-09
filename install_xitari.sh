968c42964721c8a07bdab4caeef13ef6f73bbc62
choo8 <choo8@illinois.edu>
2018-03-09 00:58:06 -0800

    Added bash script to install xitari python interface

---
+++ install_xitari.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+reposrc=git@github.com:choo8/Xitari2Player.git
+localrepo=xitari
+localrepo_vc_dir=$localrepo/.git
+
+# Build and install Python interface for Xitari
+if [! -d $localrepo_vc_dir]
+then
+	echo "Cloning Github repo"
+	git clone $reposrc $localrepo
+else
+	echo "Pulling from Github repo"
+	cd $localrepo
+	git pull $reposrc
+fi
+
+# Compile source code
+echo "Compiling source code for Xitari2Player"
+cd xitari
+cmake .
+make install
+
+# Install python module
+echo "Installing python module"
+pip install .
